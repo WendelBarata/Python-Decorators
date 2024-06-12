07471452a4e5a7804fb16a795590d8aa91648041
Wendel Barata <wnlbarata@gmail.com>
2024-06-12 15:36:33 -0300

    Final Update timeCount

--- decorators/timeCount.py
+++ decorators/timeCount.py
@@ -1,3 +1,4 @@
+# ############# ALL YOU NEED IS THE FOLLOWING FUNCTION ########################
 import time
 
 
@@ -31,6 +32,7 @@ def timer(func: callable) -> callable:
 # Here's a generic example of how to use the decorator ########################
 ###############################################################################
 
+
 # First: define a function that you want to decorate with timer
 # Second: use @timer before the function you want to decorate
 
@@ -39,6 +41,7 @@ def timer(func: callable) -> callable:
 def YourFunction(n):  # This function will be decorated with timer
     ...  # Your code here
     return n  # Return the result of the function if needed
+
 
 ###############################################################################
 # Sample function to test the timer decorator #################################
@@ -54,3 +57,43 @@ def sleeping(n: int) -> None:
 # The output of the following codes will be almost 1 second and 3 seconds
 sleeping(1)
 sleeping(3)
+
+
+###############################################################################
+# Decorator with parameter to specify the time unit ###########################
+###############################################################################
+
+
+def timerCount(timeUnit: str = "secondes"):
+    def decorator(func: callable):
+        def wrapper(*args, **kwargs):
+            startTime = time.time()
+            func(*args, **kwargs)
+            endTime = time.time()
+            timeTaken = endTime - startTime
+            if timeUnit == "secondes":
+                print(f"{func.__name__} took {timeTaken} seconds to run.")
+            elif timeUnit == "minutes":
+                print(f"{func.__name__} took {timeTaken/60} minutes to run.")
+            elif timeUnit == "hours":
+                print(f"{func.__name__} took {timeTaken/3600} hours to run.")
+        return wrapper
+    return decorator
+
+
+###############################################################################
+# Sample function to test the timer decorator with parameter ##################
+###############################################################################
+
+
+@timerCount(timeUnit="minutes")
+def sleeping(n: int) -> None:
+    time.sleep(n)
+    return None
+
+
+sleeping(1)
+sleeping(60)
+
+
+###############################################################################
