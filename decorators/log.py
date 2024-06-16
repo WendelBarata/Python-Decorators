fef8ff8a894b88c3e92a59f0c3034d52538c4c10
Wendel Barata <wnlbarata@gmail.com>
2024-06-16 06:05:23 -0300

    Final log.py

--- decorators/log.py
+++ decorators/log.py
@@ -16,3 +16,35 @@ def log(func: callable) -> callable:
         print(f"{func.__name__} returned {result}")
         return result
     return wrapper
+
+
+###############################################################################
+# Here's a generic example of how to use the decorator ########################
+###############################################################################
+
+
+# First: define a function that you want to decorate with log
+# Second: use @log before the function you want to decorate
+
+# General structure of the function that you want to decorate with log
+@log
+def YourFunction(n):  # This function will be decorated with log
+    ...  # Your code here
+    return n  # Return the result of the function if needed
+
+
+###############################################################################
+# Sample function to test the log decorator ###################################
+###############################################################################
+
+
+@log
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+add(1, 2)
+add(3, 4)
+
+
+###############################################################################
